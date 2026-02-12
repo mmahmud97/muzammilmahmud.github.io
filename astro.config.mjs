e4a1839958351a8c0938456ee218a495202867ac
Muzammil Mahmud <mmahmud97@users.noreply.github.com>
2026-02-12 03:32:55 -0500

    sitemap remove

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muzammilmahmud.github.io',
   base: '/',
-  integrations: [sitemap()],
 });
