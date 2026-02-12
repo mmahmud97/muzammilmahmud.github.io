20d34976720be8c36f468ba70100b8f45178c611
Muzammil Mahmud <mmahmud97@users.noreply.github.com>
2026-02-12 03:46:45 -0500

    fix base in config to reach all pages without seeing 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muzammilmahmud.github.io',
-  base: '/',
+  base: '/muzammilmahmud.github.io/',
 });
